c06919c580afde8cc12061ca5721869f67ba1a34
Fabrizio Bertolotto <fbertolotto72@gmail.com>
2019-10-16 06:27:18 +0200

    Completato autenticazione

---
+++ auth.js
@@ -0,0 +1,12 @@
+ Il volume nell'unit… D Š DATA
+ Numero di serie del volume: BC0A-A96D
+
+ Directory di D:\Corso_GIT\repo\MyGitRepo
+
+15/10/2019  09:10    <DIR>          .
+15/10/2019  09:10    <DIR>          ..
+15/10/2019  09:10                 0 auth.js
+15/10/2019  07:02                 0 index.html
+15/10/2019  07:02                 0 style.css
+               3 File              0 byte
+               2 Directory  276.241.076.224 byte disponibili
